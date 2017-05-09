90543336dbddb814c0b1df19408fdaf5f995881c
权融威 <370850377@qq.com>
2017-05-09 22:41:06 +0800

    A simple Face recognization program realized by ANN(Artifical Neural Network))

---
+++ featureNormalize.m
@@ -0,0 +1,6 @@
+function [ X_norm, mu ,sigma] = featureNormalize( X )
+    mu = mean(X);
+    sigma = std(X);
+    X_norm = (X - mu) ./ sigma;
+end
+
---
+++ pca.m
@@ -0,0 +1,19 @@
+function [ X_red, U ] = pca(X)
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+    [m, n] = size(X);
+    Sigma = 1 / m * X' * X;
+    [U, S] = svd( Sigma );
+    s = S * ones(n, 1);
+    total = sum(s);
+    Sum = 0;
+    for K = 1:n
+        Sum = Sum + s(K);
+        if Sum / total >= 0.999
+            break;
+        end
+    end
+    U = U(1:K, :);
+    X_red = X * U';
+end
+
